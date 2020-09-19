4a81bc7813808bb06f4a810a87d5586d44ef5020
Rishiraja <rajaravirc@gmail.com>
2020-09-19 10:32:25 +0530

    test

--- map_test.py
+++ map_test.py
@@ -5,12 +5,10 @@ def only_odd(item):
   return item % 2 != 0 
 
 my_list = [1,2,3]
-<<<<<<< HEAD
  
-=======
 your_list = [4,5,6]
->>>>>>> master
 
+print("New changes git master")
 print(list(map(lambda item: item*5,[1,2,3])))  
 
 print(list(filter(lambda item: item % 2 != 0 ,[1,2,3,5,6])))  
