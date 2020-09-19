48fb8afea83e4d613fc33d38defee9dbe71db6b4
Rishiraja <rajaravirc@gmail.com>
2020-09-19 10:27:54 +0530

    New change in master

--- map_test.py
+++ map_test.py
@@ -7,6 +7,7 @@ def only_odd(item):
 my_list = [1,2,3]
 your_list = [4,5,6]
 
+print("New changes git master")
 print(list(map(lambda item: item*5,[1,2,3])))  
 
 print(list(filter(lambda item: item % 2 != 0 ,[1,2,3,5,6])))  
